494f1cb132b1dc7ff14034f3b0b40f3e47344736
eleppala <eleppala@student.hive.fi>
2025-01-31 09:02:47 +0200

    HttpParser uptades

--- src/main.cpp
+++ src/main.cpp
@@ -42,3 +42,38 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+
+
+//debug main for http parser
+
+
+
+// int main()
+// {
+//     std::string rawDataChunk = 
+//     "POST /upload HTTP/1.1\r\n"
+//     "Host: example.com\r\n"
+//     "Content-Type: application/x-www-form-urlencoded\r\n"
+//     "Content-Length: 16\r\n"
+//     "\r\n"
+//     "name=John&age=30"
+//     "GET /index.html HTTP/1.1\r\n"
+//     "Host: example2.com\r\n";
+
+//     //Ei toimi viela kahdella requestilla, mutta pilkkoo ensimmaisesta yhden kokonaisen
+//     // std::string rawDataChunk2 = 
+//     // "Content-Type: application/secondreq\r\n"
+//     // "\r\n";
+
+    
+//     std::istringstream input(rawDataChunk);
+//     HttpParser parser;
+//     parser.readFullRequest(input);
+//     // std::istringstream input2(rawDataChunk2);
+//     // parser.readFullRequest(input2);
+
+//     //parser result for debuggin
+//     parser.display();
+
+//     return (0);
+// }
